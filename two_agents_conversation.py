eef08074aff854fff24670efea2551b988ec3da9
jayashrisathe <70744196+jayashrisathe@users.noreply.github.com>
2026-05-30 15:27:39 +0530

    Update two_agents_conversation.py

--- two_agents_conversation.py
+++ two_agents_conversation.py
@@ -1,4 +1,9 @@
 
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# Import necessary libraries
 import os
 import autogen
 import gradio as gr
@@ -12,26 +17,28 @@ import google.generativeai as genai  # Import the Google library
 load_dotenv()
 
 # Retrieve API keys from environment variables
-openai_api_key = "API_KEY"
+openai_api_key = "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"
 google_api_key = os.getenv("GOOGLE_API_KEY")  # Load the Google API key
 
 print("Setup Complete: Libraries installed and API keys loaded.")
-config_list_gemini = [
+config_list_openai = [
     {
-        "model": "gemini-2.0-flash",  # Or "gemini-pro"
-        "api_key": google_api_key,
-        "api_type": "google",  # Specify the API type for Autogen's Google integration
+        "model": "meta/llama-3.1-8b-instruct",
+        "api_key": openai_api_key,
+        "base_url": "https://integrate.api.nvidia.com/v1"
     }
 ]
 
-llm_config_gemini = {
-    "config_list": config_list_gemini,
-    "temperature": 0.6,  # Maybe slightly less randomness for strategic Chief Marketing Officer
+llm_config_openai = {
+    "config_list": config_list_openai,
+    "temperature": 0.7,  # Use a slightly higher temp for creative marketing ideas
     "timeout": 120,
 }
 message = "We need to design a cron job to update / insert 5Lacs record each day."
 architect_agent = autogen.ConversableAgent(name="AWS_services_Solution_architect", system_message="Need to design long running cron job suggest ideas", llm_config=llm_config_openai, human_input_mode="NEVER")
 devloper_agent = autogen.ConversableAgent(name="AWS_python_developer", system_message="Need to design long running cron job suggest ideas. code hanges in short ", llm_config=llm_config_openai, human_input_mode="NEVER")
 
+chat_result_openai_only = architect_agent.initiate_chat(recipient=devloper_agent, max_turns=4, message=message)
+print_chat_history(chat_result_openai_only)
 chat_result_openai_only = architect_agent.initiate_chat(recipient=devloper_agent, max_turns=4, message=message)
 print_chat_history(chat_result_openai_only)
